f72d305dd1fabf6d6895483fddbc4eef12bd6d17
InkretIvan <ivaninkret7@gmail.com>
2022-09-05 14:36:43 +0200

    stabilna adding memory verzija

--- services/rule_engine/src/main/java/agents/Nola.java
+++ services/rule_engine/src/main/java/agents/Nola.java
@@ -3,10 +3,16 @@ package agents;
 import com.mindsmiths.ruleEngine.model.Agent;
 import com.mindsmiths.telegramAdapter.TelegramAdapterAPI;
 import lombok.*;
+import com.mindsmiths.gpt3.GPT3AdapterAPI;
+import com.mindsmiths.ruleEngine.util.Log;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 public class Nola extends Agent {
+
+    private String memory = "";
 
     public Nola() {
     }
@@ -19,5 +25,33 @@ public class Nola extends Agent {
     public void sendMessage(String text) {
         String chatId = getConnections().get("telegram");
         TelegramAdapterAPI.sendMessage(chatId, text);
+    }
+
+    public void addMessageToMemory(String sender, String text){
+        memory += String.format("%s: %s\n", sender, text);
+    }
+
+    public void askGPT3() {
+        String intro = "This is a conversation between a human and an intelligent AI assistant named Nola.\n";
+        simpleGPT3Request(intro + String.join("\n", memory) + "\nNola:");
+    }
+
+    public void simpleGPT3Request(String prompt) {
+        Log.info("Prompt for GPT-3:\n" + prompt);
+        GPT3AdapterAPI.complete(
+            prompt, // input prompt
+            "text-davinci-001", // model
+            150, // max tokens
+            0.9, // temperature
+            1.0, // topP
+            1, // N
+            null, // logprobs
+            false, // echo
+            List.of("Human:", "Nola:"), // STOP words
+            0.6, // presence penalty
+            0.0, // frequency penalty
+            1, // best of
+            null // logit bias
+        );
     }
 }
